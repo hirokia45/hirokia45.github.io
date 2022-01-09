c773a5ef44e400d14d440fdf869a7093089e9a8f
hirokia45 <hiroki.amada.90@gmail.com>
2022-01-10 04:37:46 +0900

    Update main page

---
+++ _next/static/dWYUdxjT9PUnf1qNGBUjv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-d4cc3449e89e6845.js","static/chunks/pages/index-2c9aa7336740e3d3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
